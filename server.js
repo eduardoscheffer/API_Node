817648eaee9404a353d50890497010dc5826a2c1
Tiago Dias <tiagoabadedias@hotmail.com>
2023-04-15 10:59:13 -0300

    adicionada base de dados

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const baloes = require("./database/baloes.json")
 
-app.get("/", (req, res)=> {
-    res.send("Hello world!")
+app.get("/baloes", (req, res)=> {
+    res.send(baloes)
 })
 app.listen(port)
