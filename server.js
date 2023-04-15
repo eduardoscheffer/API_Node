18af4f923960e460fe784b269c0b402e5078f601
Tiago Dias <tiagoabadedias@hotmail.com>
2023-04-15 11:35:55 -0300

    Adicionado a rota /baloes/:id

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ const baloes = require("./database/baloes.json")
 app.get("/baloes", (req, res)=> {
     res.send(baloes)
 })
+
+app.get("/baloes/:id", (req, res) => {
+    const id = req.params.id
+    
+    const baloesEncontrados = baloes.filter((balao) => balao.id == id)
+
+    if (baloesEncontrados.length == 0)
+        res.status(404).send()
+    
+    const balao = baloesEncontrados[0]
+    res.send(balao)
+})
+
 app.listen(port)
